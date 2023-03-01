efdde95266ce79d95058697d92550238109b7c81
anirudhj2000 <anirudhjoshi985@gmail.com>
2023-03-01 23:26:56 +0530

    minor changes for index files and favicon

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       </div>
       <div className='text-[1rem] flex flex-row self-center mt-[30vh]'>
         <input className='mx-2 px-2 py-1 border-2' value={searchText} onChange={(e) => {setSearchText(e.target.value)}} type={'text'} />
-        <a className='bg-blue-400 rounded-[0.25rem] p-2 text-white' href={`https://www.youtube.com/results?search_query=${searchText}`}>
+        <a className='bg-blue-400 rounded-[0.25rem] p-2 text-white' target={"_blank"} href={`https://www.youtube.com/results?search_query=${searchText}`}>
           <div>Search</div>
         </a>
       </div>
